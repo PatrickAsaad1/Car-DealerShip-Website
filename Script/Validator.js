eb714340f14cf067f022543049d12dc18fdbe902
Patrick Asaad <magyasaad424@gmail.com>
2026-06-16 19:45:40 +0300

    Add password visibility toggle functionality in Validator.js

--- Script/Validator.js
+++ Script/Validator.js
@@ -1,5 +1,17 @@
 const toggleEye = document.getElementById("toggleEye");
 const passwordInput = document.getElementById("passwordInput");
+const eyeIcon = document.getElementById("eyeIcon");
+toggleEye.addEventListener("click", function () {
+  if (passwordInput.type === "password") {
+    passwordInput.type = "text";
+    eyeIcon.classList.remove("fa-eye");
+    eyeIcon.classList.add("fa-eye-slash");
+  } else {
+    passwordInput.type = "password";
+    eyeIcon.classList.remove("fa-eye-slash");
+    eyeIcon.classList.add("fa-eye");
+  }
+});
 function Check() {
   let UsernameInput = document.querySelector(".input-username");
   let Username = UsernameInput.value.trim();
