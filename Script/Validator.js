95adf684da646874d23844e8ce6ba42b43455490
Patrick Asaad <magyasaad424@gmail.com>
2026-06-16 17:59:17 +0300

    Add login validation script and update styles for improved responsiveness

---
+++ Script/Validator.js
@@ -0,0 +1,33 @@
+function Check() {
+  let UsernameInput = document.querySelector(".input-username");
+  let Username = UsernameInput.value.trim();
+  let PasswordInput = document.querySelector(".input-password");
+  let Password = PasswordInput.value.trim();
+  // Reset lines to gray at the start of every check
+  document.getElementById("Uline").style.backgroundColor = "#666";
+  document.getElementById("Pline").style.backgroundColor = "#666";
+  if (Username === "" && Password === "") {
+    console.log("ERROR: No Username or Password Entered");
+    document.getElementById("action").innerHTML =
+      "Please Enter Your Username and Password";
+    document.getElementById("Uline").style.backgroundColor = "red";
+    document.getElementById("Pline").style.backgroundColor = "red";
+    return;
+  }
+  if (Username === "") {
+    console.log("ERROR: Empty Username Input");
+    document.getElementById("action").innerHTML = "Please Enter Your Username";
+    document.getElementById("Uline").style.backgroundColor = "red";
+    return;
+  }
+  if (Password === "") {
+    console.log("ERROR: No Password Entered");
+    document.getElementById("action").innerHTML = "Please Enter Your Password";
+    document.getElementById("Pline").style.backgroundColor = "red";
+    return;
+  }
+  console.log("Login Successful");
+  document.getElementById("action").innerHTML = "Login Successful!";
+  document.getElementById("Uline").style.backgroundColor = "green";
+  document.getElementById("Pline").style.backgroundColor = "green";
+}
